3bf75dd0a23be2cea02a279caccb13d4eda14705
huzaifasad <huzaifa084567@gmail.com>
2023-11-26 01:32:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import ChatGpt from "./Chat/ChatGpt";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Pricing from "./Chat/Pricing";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "./firebase";
+import Success from "./Chat/Success";
 
-export const App = () => {
+export default function App() {
+  const [user, loading] = useAuthState(auth);
+  const [id,setid]=useState()
+ 
   return (
-    <div>
-
-      
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<ChatGpt />}
+          />
+          <Route
+            exact
+            path="/pricing"
+            element={loading ? null : user ? <Pricing /> : <Navigate to="/" />}
+          />
+          <Route
+          exact
+          path="/success"
+          element={loading ? null : user ? <Success /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,40 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        'charcoal': '#737474',
+        'pricings':'#262626',
+      },
+    },
+    letterSpacing: {
+      '1': '0em',
+      '2': '0.025em',
+      '3': '0.05em',
+      '4': '0.1em',
+    },
+    
   },
   plugins: [],
 }
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      borderColor: {
+        'charcoal': '#737474',
+        'pricings':'#262626',
+      },
+    },
+    letterSpacing: {
+      '1': '0em',
+      '2': '0.025em',
+      '3': '0.05em',
+      '4': '0.1em',
+    },
+  },
+  plugins: [],
+});
